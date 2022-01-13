69e0e90f5c76ac06dcaef9c21b0861d735111d7c
Salih Kadir Chousein <salihkdrhsyn@gmail.com>
2022-01-13 13:14:52 +0200

    update on endpoints

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import datetime
 import  db_connection
 
@@ -44,6 +44,59 @@ def index():
 
 
     return render_template("index.html", cases=cases, date=date, num_test=num_test, positivity=positivity, dias=dias,deaths=deaths)
+
+
+@app.route('/api/v1/cases/all', methods=['GET'])
+def api_casesAll():
+    cursor = db_connection.connection.cursor()
+    query = cursor.execute("SELECT R_id,cases FROM records;")
+    result_tuple = cursor.fetchall()
+    result_list = list(result_tuple)
+    result_dic = []
+    for rec in result_list:
+        line = {"Date": rec[0], "Cases": rec[1]}
+        result_dic.append(line)
+    # print(result)
+
+    return jsonify(result_dic)
+
+@app.route('/api/v1/cases/range', methods=['GET'])
+def api_casesRange():
+    range1 = request.args['range1']
+    range2 = request.args['range2']
+
+    cursor = db_connection.connection.cursor()
+    query = cursor.execute("SELECT R_id,cases FROM records WHERE R_id BETWEEN %s AND %s;", (range1,range2))
+    result_tuple = cursor.fetchall()
+    result_list = list(result_tuple)
+    result_dic = []
+    for rec in result_list:
+        line = {"Date":rec[0],"Cases":rec[1]}
+        result_dic.append(line)
+    # print(result)
+
+    return jsonify(result_dic)
+
+@app.route('/api/v1/cases/today', methods=['GET'])
+def api_casesToday():
+    cursor = db_connection.connection.cursor()
+    query = cursor.execute("SELECT R_id,cases FROM records;")
+    result_tuple = cursor.fetchall()
+    result_list = list(result_tuple)
+    result_dic = []
+
+    line = {"Date": result_tuple[len(result_tuple)-1][0], "Cases": result_tuple[len(result_tuple)-1][1]}
+    result_dic.append(line)
+    # print(result)
+
+    return jsonify(result_dic)
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
